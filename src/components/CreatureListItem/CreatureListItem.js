0048c38ea3dd5309b5399d0ee5613e885db0b074
Myron <dubtwee@gmail.com>
2021-02-04 14:20:03 -0600

    converting CreatureListItem component to a functional component

--- src/components/CreatureListItem/CreatureListItem.js
+++ src/components/CreatureListItem/CreatureListItem.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-class CreatureListItem extends Component {
+function CreatureListItem(props) {
+  const history = useHistory();
 
-  handleClickDetails = () => {
+  const handleClickDetails = () => {
     // navigate to creature details page
-    this.props.history.push(`/creature-details/${this.props.creature.id}`);
-  }
+    history.push(`/creature-details/${props.creature.id}`);
+  };
 
-  render() {
-    const {
-      creature,
-    } = this.props;
+  const { creature } = props;
 
-    return (
-      <div className="card">
-          <div className="card-bd">
-            <h4 className="cardHdg">{creature.name}</h4>
-            <p><strong>Type:</strong> {creature.type_label}</p>
-            <p className="cardSubHdg">{creature.physical_description}</p>
-          </div>
-          <div className="card-action">
-            <button
-              className="btn"
-              onClick={this.handleClickDetails}
-            >
-              SEE DETAILS
-            </button>
-          </div>
+  return (
+    <div className="card">
+      <div className="card-bd">
+        <h4 className="cardHdg">{creature.name}</h4>
+        <p>
+          <strong>Type:</strong> {creature.type_label}
+        </p>
+        <p className="cardSubHdg">{creature.physical_description}</p>
       </div>
-    );
-  }
+      <div className="card-action">
+        <button className="btn" onClick={handleClickDetails}>
+          SEE DETAILS
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default connect()(withRouter(CreatureListItem));
+export default CreatureListItem;
