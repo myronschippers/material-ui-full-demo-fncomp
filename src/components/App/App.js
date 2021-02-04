e45cc58e4626e648ffcb7fc102333cee48923bd6
Myron <dubtwee@gmail.com>
2021-02-04 13:28:13 -0600

    converting App component to a functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { connect } from 'react-redux';
+import { useStore } from 'react-redux';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
@@ -9,23 +9,18 @@ import Home from '../pages/Home/Home';
 import Details from '../pages/Details/Details';
 import Edit from '../pages/Edit/Edit';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <AppLayout>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/creature-details/:id" component={Details} />
-          <Route exact path="/creature-edit/:id" component={Edit} />
-        </AppLayout>
-      </Router>
-    );
-  }
+function App() {
+  const secondReducer = useStore('secondReducer');
+
+  return (
+    <Router>
+      <AppLayout>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/creature-details/:id" component={Details} />
+        <Route exact path="/creature-edit/:id" component={Edit} />
+      </AppLayout>
+    </Router>
+  );
 }
 
-const mapStoreToProps = (store) => {
-  return {
-    secondReducer: store.secondReducer,
-  }
-};
-export default connect(mapStoreToProps)(App);
+export default App;
