6282bfc9b18eaed6f9bfbc7d323ae1f09b610867
Myron <dubtwee@gmail.com>
2021-02-04 15:11:41 -0600

    converting Home page to a functional component

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CreatureList from '../../CreatureList/CreatureList';
 import AlertMessages from '../../AlertMessages/AlertMessages';
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Master Bestiary</h2>
-        <CreatureList />
+function Home() {
+  return (
+    <div>
+      <h2>Master Bestiary</h2>
+      <CreatureList />
 
-        <AlertMessages />
-      </div>
-    );
-  }
+      <AlertMessages />
+    </div>
+  );
 }
 
 export default Home;
