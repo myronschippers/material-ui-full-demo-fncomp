bb7ed613aaabd2a57d37415676e117ce4944c7b8
Myron <dubtwee@gmail.com>
2021-02-04 14:28:29 -0600

    converting TypeEditor component to a functional component

--- src/components/TypeEditor/TypeEditor.js
+++ src/components/TypeEditor/TypeEditor.js
@@ -1,85 +1,86 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
-class TypeEditor extends Component {
-  state = {
+function TypeEditor(props) {
+  // LOCAL STATE
+  const [editorState, setEditorState] = useState({
     selectedType: '',
     isSelecting: false,
-  }
+  });
+  // GLOBAL STATE
+  const dispatch = useDispatch();
+  const allTypes = useSelector((store) => store.allTypes);
 
-  componentDidMount() {
-    this.props.dispatch({
+  useEffect(() => {
+    dispatch({
       type: 'GET_ALL_TYPES',
     });
-  }
+  }, [dispatch]);
 
-  handleChangeTypeSelection = (event) => {
+  const handleChangeTypeSelection = (event) => {
     const newId = parseInt(event.target.value);
 
-    this.setState({
-      selectedType: newId
-    }, () => {
-      if (this.props.typeId
-        && newId !== this.props.typeId
-        && this.props.changeCallback
-      ) {
-        this.props.changeCallback(newId);
+    setEditorState(
+      {
+        selectedType: newId,
+      },
+      () => {
+        if (
+          this.props.typeId &&
+          newId !== this.props.typeId &&
+          this.props.changeCallback
+        ) {
+          props.changeCallback(newId);
+        }
       }
-    });
-  }
+    );
+  };
 
-  matchTypeById(typeId) {
+  function matchTypeById(typeId) {
     if (!typeId || typeId === ' ') {
       return null;
     }
 
-    const matchList = this.props.store.allTypes.filter((item, index) => {
+    const matchList = allTypes.filter((item, index) => {
       return item.id === typeId;
     });
 
     return matchList[0];
   }
 
-  handleClickToSelect = (event) => {
-    this.setState({
-      selectedType: this.props.typeId,
+  const handleClickToSelect = (event) => {
+    setEditorState({
+      selectedType: props.typeId,
       isSelecting: true,
     });
-  }
+  };
 
-  render() {
-    const definedType = this.matchTypeById(this.props.typeId);
+  const definedType = matchTypeById(props.typeId);
 
-    return (
-      <label className="vr vr_x2">
-        <div>Type:</div>
-        {definedType != null && !this.state.isSelecting &&
-          <div onClick={this.handleClickToSelect}>
-            {definedType.label}
-          </div>
-        }
-        {this.state.isSelecting &&
-          <select
-            value={this.state.selectedType}
-            onChange={this.handleChangeTypeSelection}
-          >
-            <option type="">Select a Type</option>
-            {this.props.store.allTypes.map((item, index) => {
-              return (
-                <option
-                  key={index}
-                  value={item.id}
-                >
-                  {item.label}
-                </option>
-              );
-            })}
-          </select>
-        }
-      </label>
-    );
-  }
+  return (
+    <label className="vr vr_x2">
+      <div>Type:</div>
+      {definedType != null && !editorState.isSelecting && (
+        <div onClick={handleClickToSelect}>{definedType.label}</div>
+      )}
+      {editorState.isSelecting && (
+        <select
+          value={editorState.selectedType}
+          onChange={handleChangeTypeSelection}
+        >
+          <option type="">Select a Type</option>
+          {allTypes.map((item, index) => {
+            return (
+              <option key={index} value={item.id}>
+                {item.label}
+              </option>
+            );
+          })}
+        </select>
+      )}
+    </label>
+  );
 }
 
-export default connect(mapStoreToProps('allTypes'))(TypeEditor);
+export default TypeEditor;
